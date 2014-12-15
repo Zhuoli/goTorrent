d90a5895afa394c5549edc4b46836d49f6432b07
Zhuoli <liang.zhu@husky.neu.edu>
2014-12-15 01:16:01 -0800

    http header range field

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 an GUI download tool wrote in GO!
 
+Step1: implement http download using concurrency to speed up
+
--- client.go
+++ client.go
@@ -1,6 +1,29 @@
-package goTorrent
+package main
 
 import (
-
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
 )
 
+func downloadFromUrl(url string) {
+	fileName := getFileName(url)
+	fmt.Println("Downloading", url, "to", fileName)
+	
+	client :=&http.Client{}
+	req,_:=http.NewRequest("GET",url,nil)
+	req.Header.Set("Range","bytes=start-end")
+}
+
+func getFileName(url string) name String{
+	tokens :=strings.Split(url,"/")
+	fileName:=tokens[len(tokens)-1]
+	return fileName
+}
+
+func main() {
+	url := "http://download.nextag.com/apache/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz"
+	downloadFromUrl(url)
+}
